8ad82fe154e667cea69039d6dcec27a639698bbb
Adriel Santos Araujo <47580872+adriel12319@users.noreply.github.com>
2025-01-23 19:33:59 -0300

    a

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist/client',
+    ssrManifest: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  ssr: {
+    noExternal: ['react-helmet-async']
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
