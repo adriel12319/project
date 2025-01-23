3d4b95948bd2f313b9518f3d0c54213be402c519
Adriel Santos Araujo <47580872+adriel12319@users.noreply.github.com>
2025-01-23 20:24:32 -0300

    c

--- pages/about/index.page.vue
+++ pages/about/index.page.vue
@@ -3,3 +3,17 @@
   <p>Showcase of using Vike with Vercel.</p>
   <p>Demo version: 9</p>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About Page',
+      meta: [
+        { name: 'description', content: 'This is the about page of the project.' },
+        { name: 'name', content: 'About Page' }
+      ]
+    }
+  }
+}
+</script>
