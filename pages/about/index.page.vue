dc9c02b819005d42d1a4206062b6b2d7111d844c
Adriel Santos Araujo <47580872+adriel12319@users.noreply.github.com>
2025-01-23 20:18:14 -0300

    a

---
+++ pages/about/index.page.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>About</h1>
+  <p>Showcase of using Vike with Vercel.</p>
+  <p>Demo version: 9</p>
+</template>
---
+++ pages/index/index.page.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Hello from Vercel</h1>
+  This page is:
+  <ul>
+    <li>Rendered to HTML.</li>
+    <li>Interactive. <Counter /></li>
+  </ul>
+</template>
+
+<script setup>
+defineHead({
+  title: 'Welcome to My Vike App',
+  meta: [
+    {
+      name: 'description',
+      content: 'A Vue.js application built with Vike and deployed on Vercel'
+    }
+  ]
+})
+</script>
+
+<script>
+import Counter from './Counter.vue'
+const components = { Counter }
+export default { components }
+</script>
