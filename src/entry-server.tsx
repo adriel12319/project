a3a0d865078d83077ba3fe743313e0320777513d
Adriel Santos Araujo <47580872+adriel12319@users.noreply.github.com>
2025-01-23 21:18:58 -0300

    t

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -3,9 +3,9 @@ import ReactDOMServer from 'react-dom/server'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
 
-export function render() {
+export function render(url: string) {
   const helmetContext = {}
-  
+
   const html = ReactDOMServer.renderToString(
     <HelmetProvider context={helmetContext}>
       <App />
