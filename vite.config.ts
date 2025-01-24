401d4582bedf5e2c9e37b3d83a95314838aa98b5
Adriel Santos Araujo <47580872+adriel12319@users.noreply.github.com>
2025-01-23 22:32:19 -0300

    d

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080
+  },
   ssr: {
     target: 'node',
     format: 'esm',
@@ -10,6 +13,8 @@ export default defineConfig({
   },
   build: {
     assetsDir: 'assets',
+    outDir: 'dist/client', // Especifica o diretório de saída para o cliente
+    manifest: true, // Gera um manifest.json para mapear os assets
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
@@ -17,7 +22,12 @@ export default defineConfig({
           const ext = info[info.length - 1];
           return `assets/[name][extname]`;
         },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
       }
     }
+  },
+  preview: {
+    port: 8080
   }
 })
